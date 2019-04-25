ab9f8c81bd41a32a1f6c1e3e9a22e348157de400
tubidge <tubidge@gmail.com>
2019-04-24 20:48:24 -0600

    Adding project files

---
+++ bamazon.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    dept_name VARCHAR(50) NOT NULL,
+    price INT(10) NOT NULL,
+    stock_qty INT(10) NOT NULL,
+    primary key (item_id)
+);
+
+INSERT INTO products 
+(product_name, dept_name, price, stock_qty)
+VALUES 
+('3 ft. Cheese Wheel', 'Kitchen/Grocery', 145, 5),
+('Infant Caribou Hide', 'Decor', 500, 9),
+('1/2 Coding Bootcamp Certificate', 'Office', 18, 35),
+('Refurbished Carpet', 'Decor', 27, 128),
+('Ted Nugent Tapestry', 'Decor', 950, 8),
+('Possibly Stolen American Express Card', 'Questionables', 5, 12498),
+("Joel's Netflix Password", 'Entertainment', 7, 100000),
+('5 Gallon Bucket of Vaseline', 'Beauty', 20, 590),
+('Breaking Bad S3E6 DVD', 'Entertainment', 3, 821),
+('Twist Tie', 'Kitchen/Grocery', 1, 17);
---
+++ bamazonCustomer.js
@@ -0,0 +1,10 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'R0otsNh0ots9',
+    database: 'bamazon_db'
+});

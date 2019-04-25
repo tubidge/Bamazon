c5b1e81dd2a8f03dad3c2926199c6562b65304b1
tubidge <tubidge@gmail.com>
2019-04-24 21:02:16 -0600

    node app displaying inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,3 +8,26 @@ var connection = mysql.createConnection({
     password: 'R0otsNh0ots9',
     database: 'bamazon_db'
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`\nconnected as id ${connection.threadId}`);
+    dispInventory();
+    // startPrompt();
+});
+
+function dispInventory() {
+    var query = 'SELECT item_id, product_name, price FROM products';
+    connection.query(query, function (err, res) {
+        console.log('Current Inventory: \n-------------------')
+        for (var i = 0; i < res.length; i++) {
+            console.log(`Item ID: ${res[i].item_id} | Item Name: ${res[i].product_name} | Item Price: $${res[i].price}`);
+        }
+    });
+
+};
+
+
+// function startPrompt() {
+
+// };
